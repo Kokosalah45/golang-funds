04e937e3ef971ad7b1d6012be7607b45e36c2bf7
Kokosalah45 <kokp.amged@gmail.com>
2023-04-11 21:14:46 +0200

    feature:savetofile/readfromfile/dealing hands

--- deck.go
+++ deck.go
@@ -1,30 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"math/rand"
+	"os"
 )
 
 type card struct {
-	suit  string
-	value string
+	Suit  string `json:"suit"`
+	Value string `json:"value"`
 }	
 
 type deck []card
 
-func (dck deck) list() {
 
+
+func generateDeck() deck {
+	dck  := deck{}
+	suits := [4]string{"Diamonds" , "Spades" , "Hearts" , "Clubs"}
+ 	values := [13]string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten" , "King", "Jack", "Queen"}	
+	for _, suit := range suits{
+		for _, value := range values{
+			card := card{suit , value}
+			dck = append(dck, card)
+		}
+	}
+	return dck
+}
+
+func (dck deck) list() {
 	for _, card := range dck {
-		fmt.Println(card.value , "of" , card.suit)
+		fmt.Println(card.Suit + " of " + card.Value)
 	
 	}
 	fmt.Println("---------------");
 }
 
-func getRandomNumbers(upperBound int) (int,int) {
-	x , y := rand.Intn(upperBound) , rand.Intn(upperBound)
-	return x,y
+func getRandomNumbers(upperBound int) [2]int {
+	randNums := [2]int{rand.Intn(upperBound) , rand.Intn(upperBound)}
+	return randNums
 }
+
+func (dck *deck) deal(handSize int) deck {
+	hand := (*dck)[:handSize]
+	*dck = (*dck)[handSize:]
+	return hand
+} 
+
 
 
 func (dck *deck) shuffle(rounds int) {
@@ -32,17 +55,52 @@ func (dck *deck) shuffle(rounds int) {
 	temp := *dck;
 	i := 1
 	for i <= rounds {
-		var randIndx1 , randIndx2 int 
+		var randIndxs [2]int
 		// needs optimizting if either bounds do something while in middle do different thing
 	    for {
-			randIndx1 , randIndx2 =  getRandomNumbers(dckLen)
-			if (randIndx1 != randIndx2){
+			randIndxs =  getRandomNumbers(dckLen)
+			if (randIndxs[0] != randIndxs[1]){
 				break
 			}
 		}
-		temp[randIndx1] , temp[randIndx2] = temp[randIndx2] , temp[randIndx1]
+		temp[randIndxs[0]] , temp[randIndxs[1]] = temp[randIndxs[1]] , temp[randIndxs[0]]
 		i = i + 1;
 	}
+}
+
+func (dck deck) saveToFile(filename string){
+	stringDeck := dck.stringify()
+	err := os.WriteFile(filename , stringDeck , 0644)
+	if (err != nil){
+		panic(err)
+	}
+}
+func readFromFile(filename string) deck {
 	
+	stringifiedText , err := os.ReadFile(filename)
 	
+	if (err != nil){
+		panic(err)
+	}
+	return parse(stringifiedText)
+}
+
+
+
+func (dck deck) stringify() []byte {
+	jsonDeck , err := json.Marshal(dck)
+	if (err != nil){
+		panic(err)
+	}
+	return jsonDeck
+}
+
+func parse(stringifiedDeck []byte) deck{
+	var dck deck ;
+	err := json.Unmarshal(stringifiedDeck,&dck)
+	 if (err != nil){
+		panic(err)
+	}
+	fmt.Println(dck)
+	return dck
 }
--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
-	deck := deck{
-		{value: "king", suit: "spades"},
-		{value: "eleven", suit: "hearts"},
-		{value: "three", suit: "diamonds"},
-		{value: "four", suit: "clubs"},
-		{value: "queen", suit: "hearts"}}
-	deck.list()
-	deck.shuffle(3)
-	deck.list()
-	deck.shuffle(3)
-	deck.list()
+	dck := generateDeck()
+	dck.shuffle(100)
+	hand1 := dck.deal(7)
+	fmt.Println(len(dck))
+	hand2 := dck.deal(7)
+	fmt.Println(len(dck))
+	hand3 := dck.deal(7)
+	fmt.Println(hand1)
+	fmt.Println(hand2)
+	fmt.Println(hand3)
+	fmt.Println(len(dck))
+	dck.saveToFile("./test.txt")
+	loadedck := readFromFile("./test.txt")
+	fmt.Printf("len original : %d / len loaded : %d", len(dck) , len(loadedck))
+
+
 }
